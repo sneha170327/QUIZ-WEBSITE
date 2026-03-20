78762cf55e37db056cf8b22e999c9575fdae4d69
sneha170327 <sp3553731@gmail.com>
2026-03-20 11:34:03 +0530

    Update quiz.php

--- quiz.php
+++ quiz.php
@@ -1,14 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Quiz</title>
+    <meta name="description" content="Take a simple quiz on HTML, CSS, and PHP. Test your basic web development knowledge.">
+
 <link rel="stylesheet" href="index.css">
 </head>
 
 <body>
-
+<header>
 <h2 style="text-align:center;">Simple Quiz</h2>
-
+</header>
 <form action="result.php" method="POST">
 
 <p>1. HTML stands for?</p>
